9028b834c43bdb946e2523676e94cef4df35a8c2
Arpan Dixit <dixit.arpan20@gmail.com>
2026-08-18 00:29:23 +0530

    fix: align all FullCalendar packages to v6.1.21 to resolve class constructor crash

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react({
-      // Exclude FullCalendar from Babel class transforms to fix:
-      // "Class constructor X cannot be invoked without 'new'"
-      exclude: [/node_modules\/@fullcalendar/],
-    }),
-  ],
+  plugins: [react()],
   optimizeDeps: {
     include: [
       '@fullcalendar/core',
@@ -30,4 +24,5 @@ export default defineConfig({
     setupFiles: './src/test/setupTests.ts',
     css: false,
   },
-});
+});
+
